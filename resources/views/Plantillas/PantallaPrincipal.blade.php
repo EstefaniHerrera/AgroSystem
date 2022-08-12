09f1f279cd4a2b036b08c6d3366a4fa89aa711be
belsy05 <belsy.mairena@unah.hn>
2022-08-12 17:32:46 -0600

    Update PantallaPrincipal.blade.php

--- resources/views/Plantillas/PantallaPrincipal.blade.php
+++ resources/views/Plantillas/PantallaPrincipal.blade.php
@@ -222,6 +222,34 @@
       aria-hidden="true">
       <div class="modal-dialog" role="document">
           <div class="modal-content">
+<<<<<<< Updated upstream
+
+              <div class="modal-header" style="background: white; color: black">
+                  <h5 class="modal-title" id="exampleModalLabel" ><br><strong>Cerrar sesión</strong></h5>
+              </div>
+              <div class="modal-body">¿Está seguro que desea cerrar la sesión? <br>
+                Presione si para salir o presione no para cancelar</div>
+              <div class="modal-footer">
+                <div class="row" style="width: 78%">
+                  <div class="col-sm-6">
+                    <form action="{{ route('logout') }}" method="POST">
+                      @csrf
+                      <button class="btn btn-primary  btn-block" type="submit">
+                          {{ __('Si') }}
+                      </button>
+                  </div>
+                  <div class="col-sm-6">
+                    <button id="Boton1" class="btn btn-danger btn-block" type="button" data-dismiss="modal">No</button>
+                  </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+=======
+>>>>>>> Stashed changes
 
               <div class="modal-header" style="background: white; color: black">
                   <h5 class="modal-title" id="exampleModalLabel" ><br><strong>Cerrar sesión</strong></h5>
@@ -248,6 +276,43 @@
 
 
 
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+
+              <div class="modal-header" style="background: white; color: black">
+                  <h5 class="modal-title" id="exampleModalLabel" ><br><strong>Cerrar sesión</strong></h5>
+              </div>
+              <div class="modal-body">¿Está seguro que desea cerrar la sesión? <br>
+                Presione si para salir o presione no para cancelar</div>
+              <div class="modal-footer">
+                <div class="row" style="width: 78%">
+                  <div class="col-sm-6">
+                    <form action="{{ route('logout') }}" method="POST">
+                      @csrf
+                      <button class="btn btn-primary  btn-block" type="submit">
+                          {{ __('Si') }}
+                      </button>
+                  </div>
+                  <div class="col-sm-6">
+                    <button id="Boton1" class="btn btn-danger btn-block" type="button" data-dismiss="modal">No</button>
+                  </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+>>>>>>> ff4bf5c7223a9c9cae6490d0ebb96add932be692
+
+    <div class="col-sm-2">
+      <h2 class=""><strong>Personal</strong></h2>
+          <a href="/personals">
+            <img src="Imagenes/personal.jpeg" class="img-circle person">
+          </a>
+    </div>
 
 <!-- Footer -->
 <footer class="text-center">
