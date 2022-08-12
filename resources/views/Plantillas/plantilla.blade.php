1d03420bf9c7ce6f655ea2b17313c048bb055048
MarilynMolina <84828279+MarilynMolina@users.noreply.github.com>
2022-08-12 16:45:20 -0600

    Update plantilla.blade.php

--- resources/views/Plantillas/plantilla.blade.php
+++ resources/views/Plantillas/plantilla.blade.php
@@ -458,14 +458,51 @@
                     <span></span>
                 </button>
             </div>
-            <nav class="navbar navbar-default" id="barras">
+          <nav class="navbar navbar-default" id="barras">
 
                 <div class="container-fluid">
 
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
                             {{-- <li><a href="#">Page</a></li> --}}
-                            
+                            <li>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#Informacion">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Información
+                                </a>
+                            </li>
+                            <li>
+                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600">{{ Auth::user()->name }}</span>
+                                    <img class="img-profile rounded-circle" src="" alt="">
+                                </a>
+                                <!-- Dropdown - User Information -->
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                    aria-labelledby="userDropdown">
+                                    <a class="dropdown-item" href="#" data-toggle="modal"
+                                        data-target="#Contrasena">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Cambiar contraseña
+                                    </a>
+                                    {{-- <br>
+                                        <a class="dropdown-item" href="#" >
+                                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Información
+                                        </a>
+                                        <br> --}}
+                                    <br>
+                                    <a class="dropdown-item" href="#" data-toggle="modal"
+                                        data-target="#logoutModal">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Cerrar sesión
+                                    </a>
+                                </div>
+                            </li>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </ul>
                     </div>
                 </div>
