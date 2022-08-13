91507a56cfa2a2c8ce2f2f02ee133e9ec5105c12
EstefaniHerrera <estefani.herrera@unah.hn>
2022-08-12 18:09:56 -0600

    Update raizpersonal.blade.php

--- resources/views/Personal/raizpersonal.blade.php
+++ resources/views/Personal/raizpersonal.blade.php
@@ -41,6 +41,15 @@
         } 
         
     </style> 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
     @if (session('mensaje'))
         <div class="alert alert-success">
             {{ session('mensaje') }}
