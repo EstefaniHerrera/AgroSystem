b67e16c5bb905b74453217fe8490aea31849d9b8
EstefaniHerrera <estefani.herrera@unah.hn>
2022-08-12 18:07:34 -0600

    Update precios.blade.php

--- resources/views/Inventario/precios.blade.php
+++ resources/views/Inventario/precios.blade.php
@@ -1,6 +1,15 @@
 @extends('Plantillas.plantilla')
 @section('titulo', 'Precios')
 @section('contenido')
+@if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @if (session('mensaje'))
         <div class="alert alert-success">
             {{ session('mensaje') }}
