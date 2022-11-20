a885dff38347068a0c36c6ee7d731b618da2795f
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-11-19 20:42:59 -0600

    Update PersonalTableSeeder.php

--- database/seeders/PersonalTableSeeder.php
+++ database/seeders/PersonalTableSeeder.php
@@ -15,6 +15,21 @@ class PersonalTableSeeder extends Seeder
      */
     public function run()
     {
-        Personal::Factory(20)->create();
+        Personal::create(
+            [
+                'cargo_id'  => '1',
+                'IdentidadDelEmpleado' =>'0000000000000',
+                'NombresDelEmpleado' => 'Administrador',
+                'ApellidosDelEmpleado' => 'Primero',
+                'CorreoElectrónico' => 'primerusuario@arreiro.com',
+                'Teléfono' => '00000000',
+                'FechaDeNacimiento' => '2000-12-02',
+                'FechaDeIngreso'  => '2022-08-01',
+                'Ciudad' => 'Danlí',
+                'Dirección' => 'San Diego, Jamastran',
+                'EmpleadoActivo' => 'Activo'
+            ]
+        );
+
     }
 }
