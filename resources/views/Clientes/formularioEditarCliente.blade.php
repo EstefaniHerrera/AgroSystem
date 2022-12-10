13b80ad76bc96d9800cb63689aa1b358b5556242
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-12-10 17:12:51 -0600

    Update formularioEditarCliente.blade.php

--- resources/views/Clientes/formularioEditarCliente.blade.php
+++ resources/views/Clientes/formularioEditarCliente.blade.php
@@ -28,7 +28,9 @@
         <label for="IdentidadDelCliente"> Identidad </label>
         <input type="tel" class="form-control" name="IdentidadDelCliente" id="IdentidadDelCliente"
         placeholder="Identidad del cliente sin guiones" pattern="[0-1][0-8][0-2][0-9]{10}" required 
-        value="{{old('IdentidadDelCliente' , $cliente->IdentidadDelCliente)}}" maxlength="13" title="La identidad debe comenzar con 0 o con 1. Debe ingresar 13 caracteres">
+        value="{{old('IdentidadDelCliente' , $cliente->IdentidadDelCliente)}}" maxlength="13" title="La identidad debe comenzar con 0 o con 1. Debe ingresar 13 caracteres"
+         {{-- # 30 Llamada a la funcion para que solo tome numeros  --}}
+        onkeypress="return valideKey(event);">
     </div>
 
     <div class="form-group">
@@ -46,13 +48,16 @@
     <div class="form-group">
         <label for="Telefono"> Teléfono </label>
         <input type="tel" class="form-control" name="Telefono" id="Telefono" placeholder="00000000"
-        pattern="([2-3, 8-9][0-9]{7})" value="{{old('Telefono', $cliente->Telefono)}}" maxlength="8" title="El teléfono debe comenzar con 2, 3, 8 o 9. Debe ingresar 8 caracteres">
+        pattern="([2-3, 8-9][0-9]{7})" value="{{old('Telefono', $cliente->Telefono)}}" maxlength="8" title="El teléfono debe comenzar con 2, 3, 8 o 9. Debe ingresar 8 caracteres"
+        {{-- # 31 Llamada a la funcion para que solo tome numeros  --}}
+        onkeypress="return valideKey(event);">
     </div>
 
     <div class="form-group">
         <label for="LugarDeProcedencia"> Dirección </label>
         <input type="text" class="form-control" name="LugarDeProcedencia" id="LugarDeProcedencia" required
-        placeholder="Lugar de Procedencia" maxlength="150" value="{{old('LugarDeProcedencia', $cliente->LugarDeProcedencia)}}">
+        {{-- # 32 se corrigio el maxlength para que coincida con los establecido en el controlador --}}
+        placeholder="Lugar de Procedencia" maxlength="120" value="{{old('LugarDeProcedencia', $cliente->LugarDeProcedencia)}}">
     </div>
 
     <br>
@@ -64,6 +69,22 @@
 
 @endsection
 @push('alertas')
+
+ {{-- # 30, 31 Funcion para que solo tome numeros --}}
+    <script type="text/javascript"> 
+        function valideKey(evt){    
+            // code is the decimal ASCII representation of the pressed key.
+                var code = (evt.which) ? evt.which : evt.keyCode;
+            if(code==8) { // backspace.
+                return true;
+            } else if(code>=48 && code<=57) { // is a number.
+                return true;
+            } else{ // other keys.
+                return false;
+            }
+        }
+    </script>
+
     <script>
         function restaurar() {
             $("#IdentidadDelCliente").val('{{$cliente->IdentidadDelCliente}}');
@@ -72,6 +93,7 @@
             $("#Telefono").val('{{$cliente->Telefono}}');
             $("#LugarDeProcedencia").val('{{$cliente->LugarDeProcedencia}}');
         }
+        
         function confirmar() {
            var formul = document.getElementById("form_editarC");
             Swal.fire({
