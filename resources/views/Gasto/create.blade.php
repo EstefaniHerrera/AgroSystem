ed36c5957afdee1933ea87d5ad8c827f2738dcbe
EstefaniHerrera <estefani.herrera@unah.hn>
2022-12-11 13:51:45 -0600

    Update create.blade.php

--- resources/views/Gasto/create.blade.php
+++ resources/views/Gasto/create.blade.php
@@ -40,7 +40,8 @@
 
     <div class="form-group">
         <label for="descripcion"> Descripción </label>
-        <textarea name="descripcion" id="descripcion" placeholder="Descripción del gasto" class="form-control" maxlength="150" minlength="5" required cols="30" rows="6">{{ old('descripcion') }}</textarea>
+        <textarea name="descripcion" id="descripcion" placeholder="Descripción del gasto" class="form-control" 
+        {{--#69 Corrección del maxlength de 200 a 150--}}maxlength="150" minlength="5" required cols="30" rows="6">{{ old('descripcion') }}</textarea>
     </div>
 
     <div class="form-group">
@@ -67,8 +68,9 @@
     <div class="form-group">
         <label for="total"> Total </label>
         <input type="number" class="form-control" name="total" id="total" 
-        placeholder="Total del gasto" value="{{ old('total') }}" required step="any" min="1" max="50000" minlength="1" 
-        maxlength="7" oninput="maxlengthNumber(this);">
+        placeholder="Total del gasto" value="{{ old('total') }}" required step="any" min="1" max="50000" 
+        {{--#68 Corrección colocación de maxlength--}}
+        minlength="1" maxlength="7" oninput="maxlengthNumber(this);">
     </div>
 
     <br>
@@ -81,6 +83,7 @@
 @endsection
 @push('alertas')
 <script>
+    /* #68 Corrección colocación de maxlength */
     function maxlengthNumber(obj){
         if(obj.value.length > obj.maxLength){
             obj.value = obj.value.slice(0, obj.maxLength);
