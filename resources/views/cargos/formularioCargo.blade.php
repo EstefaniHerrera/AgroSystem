a8930d6f0f47236e9def2e484d6539ca2ff8e8a0
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-12-11 15:13:52 -0600

    Update formularioCargo.blade.php

--- resources/views/cargos/formularioCargo.blade.php
+++ resources/views/cargos/formularioCargo.blade.php
@@ -31,11 +31,12 @@
         <input type="text" class="form-control" name="NombreDelCargo" id="NombreDelCargo" placeholder="Nombre del cargo" 
         maxlength="40" value="{{old('NombreDelCargo')}}" required>
     </div>
-    {{-- # 2 Se establecio el maxlength y el minlength --}}
+    
+    {{-- # 2 Se corrigio el maxlength para que coincida con lo establecido en el controlador --}}
     <div class="form-group">
         <label for="DescripciónDelCargo"> Descripción </label>
         <textarea class="form-control" name="DescripciónDelCargo" id="DescripciónDelCargo" cols="30" rows="10" 
-        placeholder="Breve descripción de la función del puesto" maxlength="200" minlength="5" required>{{old('DescripciónDelCargo')}}</textarea>
+        placeholder="Breve descripción de la función del puesto" maxlength="110" minlength="5" required>{{old('DescripciónDelCargo')}}</textarea>
     </div>
 
     <div class="form-group">
