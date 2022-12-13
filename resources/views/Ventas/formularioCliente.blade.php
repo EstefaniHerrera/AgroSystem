bcadbefb3553ca4903dcb8963420e4ba80ef8d64
KarlaSierra <Kasierrap@unah.hn>
2022-12-12 18:59:25 -0600

    Update formularioCliente.blade.php
    Correcciones formulario cliente

--- resources/views/Ventas/formularioCliente.blade.php
+++ resources/views/Ventas/formularioCliente.blade.php
@@ -30,7 +30,9 @@
         <label for="IdentidadDelCliente"> Identidad </label>
         <input type="tel" class="form-control" name="IdentidadDelCliente" maxlength="13" id="IdentidadDelCliente"
         placeholder="Identidad del cliente sin guiones" pattern="[0-1][0-8][0-2][0-9]{10}" 
-        required value="{{old('IdentidadDelCliente')}}" title="La identidad debe comenzar con 0 o con 1. Debe ingresar 13 caracteres">
+        required value="{{old('IdentidadDelCliente')}}" title="La identidad debe comenzar con 0 o con 1. Debe ingresar 13 caracteres"
+        {{-- # 25 Llamada a la funcion para que solo tome numeros  --}}
+        onkeypress="return valideKey(event);">
     </div>
 
     <div class="form-group">
@@ -48,13 +50,16 @@
     <div class="form-group">
         <label for="Telefono"> Teléfono </label>
         <input type="tel" class="form-control" name="Telefono" id="Telefono" placeholder="00000000"
-        pattern="([2-3, 8-9][0-9]{7})" value="{{old('Telefono')}}" maxlength="8" title="El teléfono debe comenzar con 2, 3, 8 o 9. Debe ingresar 8 caracteres">
+        pattern="([2-3, 8-9][0-9]{7})" value="{{old('Telefono')}}" maxlength="8" title="El teléfono debe comenzar con 2, 3, 8 o 9. Debe ingresar 8 caracteres"
+        {{-- # 26 Llamada a la funcion para que solo tome numeros  --}}
+        onkeypress="return valideKey(event);">
     </div>
 
     <div class="form-group">
         <label for="LugarDeProcedencia"> Dirección </label>
         <input type="text" class="form-control" name="LugarDeProcedencia" id="LugarDeProcedencia"
-        placeholder="Lugar de Procedencia" maxlength="150" value="{{old('LugarDeProcedencia')}}" required>
+        {{-- # 27 se corrigio el maxlength para que coincida con los establecido en el controlador --}}
+        placeholder="Lugar de Procedencia" maxlength="70" value="{{old('LugarDeProcedencia')}}" required>
     </div>
 
     <br>
@@ -66,6 +71,22 @@
 
 @endsection
 @push('alertas')
+
+  {{-- # 25, 26 Funcion para que solo tome numeros --}}
+  <script type="text/javascript"> 
+    function valideKey(evt){    
+        // code is the decimal ASCII representation of the pressed key.
+            var code = (evt.which) ? evt.which : evt.keyCode;
+        if(code==8) { // backspace.
+            return true;
+        } else if(code>=48 && code<=57) { // is a number.
+            return true;
+        } else{ // other keys.
+            return false;
+        }
+    }
+    </script>
+
     <script>
         function confirmar() {
            var formul = document.getElementById("form_guardarC");
